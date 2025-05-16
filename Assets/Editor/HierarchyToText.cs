e67503882250c3b75e4a70fac446bff221316bc9
GGongchiKim <dkdlrh213@naver.com>
2025-05-16 11:52:57 +0900

    UIadvance

---
+++ Assets/Editor/HierarchyToText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Text;
+
+public class HierarchyToText : EditorWindow
+{
+    [MenuItem("Tools/Export Hierarchy as Text")]
+    public static void ShowWindow()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            PrintHierarchy(obj.transform, 0, sb);
+        }
+
+        string output = sb.ToString();
+        EditorGUIUtility.systemCopyBuffer = output;
+        Debug.Log("Hierarchy copied to clipboard:\n" + output);
+    }
+
+    private static void PrintHierarchy(Transform t, int indent, StringBuilder sb)
+    {
+        sb.AppendLine($"{new string(' ', indent * 2)}└── {t.name}");
+        foreach (Transform child in t)
+        {
+            PrintHierarchy(child, indent + 1, sb);
+        }
+    }
+}
